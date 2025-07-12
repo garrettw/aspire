d1eb32f0f271bb6836a0c0b7b240acb960194124
Garrett W <gw@garrettw.net>
2025-07-11 22:16:44 -0500

    delete old trash

--- other/tw/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require 'tw-config.php'; // Define configuration constants
-require 'core/tw-init.php'; // Set up environment
-
-$tw = $dic->create('Talkwork\\App');
-$tw->run();
-echo $tw->out();
